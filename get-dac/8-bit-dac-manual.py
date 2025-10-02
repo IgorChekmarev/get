b5d3259821954fa5cce41adcaa4336b9bbe68350
Чекмарев Игорь <idchekmarev@gmail.com>
2025-10-02 15:20:56 +0300

    Исправил визуальную часть ошибочных вводов

--- get-dac/8-bit-dac-manual.py
+++ get-dac/8-bit-dac-manual.py
@@ -3,10 +3,10 @@ GPIO.setmode(GPIO.BCM)
 dac_bits = [16, 20, 21, 25, 26, 17, 27, 22]
 GPIO.setup(dac_bits, GPIO.OUT)
 GPIO.output(dac_bits, 0)
-dynamic_range = 3.3
+dynamic_range = 3.14
 def voltage_to_number(voltage):
     if not (0.0 <= voltage <= dynamic_range):
-        print(f"Напряжение выходит за динамический диапазон ЦАП (0.00 - {dynamic_range: .2f} В")
+        print(f"Напряжение выходит за динамический диапазон ЦАП (0.00 - {dynamic_range: .2f}) В")
         print("устанавливаем 0.0 В")
         return 0
     return int(voltage / dynamic_range * 255)

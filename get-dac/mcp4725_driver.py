d5b262bcc103105275b3394d477993686b762ce3
Чекмарев Игорь <idchekmarev@gmail.com>
2025-10-09 16:33:34 +0300

    Доделал 12-битный I2C ЦАП

--- get-dac/mcp4725_driver.py
+++ get-dac/mcp4725_driver.py
@@ -31,16 +31,16 @@ class MCP4725:
     def set_voltage(self, voltage):
         if not (0.0 <= voltage <= self.dynamic_range):
             print(f"Напряжение выходит за динамический диапазон ЦАП (0.00 - {self.dynamic_range:.2f}) В")
-            return
+            print("Устанавливаем 0.0 В")
+            voltage = 0.0
         
-        # Преобразуем напряжение в цифровое значение (12 бит)
-        # voltage / dynamic_range = number / 4095
+
         number = int((voltage / self.dynamic_range) * 4095)
         
-        # Ограничиваем значение 12 битами
+
         number = max(0, min(number, 4095))
         
-        # Устанавливаем значение через set_number
+
         self.set_number(number)
         
         if self.verbose:
@@ -48,11 +48,11 @@ class MCP4725:
 
 if __name__ == "__main__":
     try:
-        # Создаем объект MCP4725 вместо PWM_DAC
-        dac = MCP4725(dynamic_range=3.28, address=0x61, verbose=True)
+
+        dac = MCP4725(5.15, 0x61, True)
         while True:
             try:
-                user_input = input("Введите напряжение в вольтах: ").strip().replace(',', '.')
+                user_input = input("Введите напряжение в вольтах: ")
                 voltage = float(user_input)
                 dac.set_voltage(voltage)
 

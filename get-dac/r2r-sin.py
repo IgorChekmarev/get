32192e8347cce03dc709eb96eb8778010cb5f799
Чекмарев Игорь <idchekmarev@gmail.com>
2025-10-23 15:18:11 +0300

    r2r_adc

--- get-dac/r2r-sin.py
+++ get-dac/r2r-sin.py
@@ -1,19 +1,22 @@
 import r2r_dac as r2r
 import signal_generator as sg
 import time
-amplitude = 3.2
+amplitude = 3.14
 signal_frequency = 10
-sampling_frequency = 1000
-import RPi.GPIO as GPIO
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(8, GPIO.OUT)
+sampling_frequency = 1000 
+start_time = 0
 if __name__ == "__main__":
     try:
-        DAC = R2R_DAC([16, 20, 21, 25, 26, 17, 27, 22], 3.14, True)
+        DAC = r2r.R2R_DAC([16, 20, 21, 25, 26, 17, 27, 22], 3.14, True)
 
         while True:
             try:
-                sg.get_sin_wave_amplitude(frequency, sg.wait_for_sampling_period(sampling_frequency))
-
+                end_time = time.time()
+                elapsed_time = end_time-start_time
+                a = sg.get_sin_wave_amplitude(signal_frequency, elapsed_time)
+                DAC.set_voltage(amplitude*a)
+                sg.wait_for_sampling_period(sampling_frequency)
+            except ValueError:
+                print("Вы ввели не число. Попробуйте еще раз\n")
     finally:
-        dac.deinit()
+        DAC.deinit()
